95fb6d9d6fbaeea803eff78330519aa1cd672854
mzpbvsig <972403325@qq.com>
2019-07-11 15:07:40 +0800

    add sgamesdk Singleton and Cnfig

--- app/src/main/java/com/yc/sgame/core/SGameSDK.java
+++ app/src/main/java/com/yc/sgame/core/SGameSDK.java
@@ -3,9 +3,25 @@ package com.yc.sgame.core;
 import android.content.Context;
 
 public class SGameSDK implements ISGameSDK {
+    
+    private static SGameSDK sGameSDK;
+
+    public static SGameSDK getImpl() {
+        if (sGameSDK == null) {
+            synchronized (SGameSDK.class) {
+                if (sGameSDK == null) {
+                    sGameSDK = new SGameSDK();
+                }
+            }
+        }
+        return sGameSDK;
+    }
+
+    private Config mConfig;
+
     @Override
     public void init(Context context, Config config) {
-
+        this.mConfig = config;
     }
 
     @Override
